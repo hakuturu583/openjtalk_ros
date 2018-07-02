b1ceae2b53460c84ef7f3c8ef25913eeb0a09d04
Masaya Kataoka <ms.kataoka@gmail.com>
2018-07-02 23:15:18 +0900

    add tts_action server

--- src/text_to_speech.cpp
+++ src/text_to_speech.cpp
@@ -1,7 +1,7 @@
 //headers in this package
 #include <openjtalk_ros/text_to_speech.h>
 
-text_to_speech::text_to_speech() : params_(), tts_action_server_(nh_, params_.tts_goal_topic, boost::bind(&text_to_speech::tts_goal_callback_, this, _1), false)
+text_to_speech::text_to_speech() : params_(), tts_action_server_(nh_, params_.tts_goal_topic, boost::bind(&text_to_speech::tts_goal_callback_, this, _1), true)
 {
     
 }
