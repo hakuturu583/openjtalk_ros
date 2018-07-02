1e86cee8ef8ade7b815320f0c12a46e706998c72
Masaya Kataoka <ms.kataoka@gmail.com>
2018-07-02 23:07:42 +0900

    modify CMakeLists.txt

--- include/openjtalk_ros/text_to_speech.h
+++ include/openjtalk_ros/text_to_speech.h
@@ -8,6 +8,9 @@
 //headers in this package
 #include <openjtalk_ros/TextToSpeechAction.h>
 
+//headers in STL
+#include <sys/stat.h>
+
 class text_to_speech
 {
     public:
@@ -18,7 +21,7 @@ class text_to_speech
             parameters()
             {
                 ros::param::param<std::string>(ros::this_node::getName()+"/voice_file_path", voice_file_path, "");
-                ros::param::param<std::string>(ros::this_node::getName()+"/tts_goal_topic", tts_goal_topic, "");
+                ros::param::param<std::string>(ros::this_node::getName()+"/tts_goal_topic", tts_goal_topic, ros::this_node::getName()+"/goal");
             }
         };
         text_to_speech();
@@ -28,6 +31,8 @@ class text_to_speech
         ros::NodeHandle nh_;
         actionlib::SimpleActionServer<openjtalk_ros::TextToSpeechAction> tts_action_server_;
         void tts_goal_callback_(const openjtalk_ros::TextToSpeechGoalConstPtr &goal);
+        openjtalk_ros::TextToSpeechFeedback feedback_;
+        openjtalk_ros::TextToSpeechResult result_;
 };
 
 #endif  //TEXT_TO_SPEECH_H_INCLUDED
--- src/text_to_speech.cpp
+++ src/text_to_speech.cpp
@@ -3,7 +3,7 @@
 
 text_to_speech::text_to_speech() : params_(), tts_action_server_(nh_, params_.tts_goal_topic, boost::bind(&text_to_speech::tts_goal_callback_, this, _1), false)
 {
-
+    
 }
 
 text_to_speech::~text_to_speech()
