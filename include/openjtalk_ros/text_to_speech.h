2c68590087b7b652aff29b636279667f91a0120f
Masaya Kataoka <ms.kataoka@gmail.com>
2018-07-02 22:52:56 +0900

    add parameters for tts class

--- include/openjtalk_ros/text_to_speech.h
+++ include/openjtalk_ros/text_to_speech.h
@@ -1,4 +1,33 @@
 #ifndef TEXT_TO_SPEECH_H_INCLUDED
 #define TEXT_TO_SPEECH_H_INCLUDED
 
+//headers in ROS
+#include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
+
+//headers in this package
+#include <openjtalk_ros/TextToSpeechAction.h>
+
+class text_to_speech
+{
+    public:
+        struct parameters
+        {
+            std::string voice_file_path;
+            std::string tts_goal_topic;
+            parameters()
+            {
+                ros::param::param<std::string>(ros::this_node::getName()+"/voice_file_path", voice_file_path, "");
+                ros::param::param<std::string>(ros::this_node::getName()+"/tts_goal_topic", tts_goal_topic, "");
+            }
+        };
+        text_to_speech();
+        ~text_to_speech();
+    private:
+        parameters params_;
+        ros::NodeHandle nh_;
+        actionlib::SimpleActionServer<openjtalk_ros::TextToSpeechAction> tts_action_server_;
+        void tts_goal_callback_(const openjtalk_ros::TextToSpeechGoalConstPtr &goal);
+};
+
 #endif  //TEXT_TO_SPEECH_H_INCLUDED
--- src/text_to_speech_node.cpp
+++ src/text_to_speech_node.cpp
@@ -1,8 +1,13 @@
+//headers in this package
+#include <openjtalk_ros/text_to_speech.h>
+
+//headers in ROS
 #include <ros/ros.h>
 
 int main(int argc, char** argv)
 {
     ros::init (argc, argv, "text_to_speech_node");
+    text_to_speech tts;
     ros::spin();
     return 0;
 }
